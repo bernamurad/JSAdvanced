931c143fb3b8965e6f93019cf34775de2508df67
bernamurad <76744453+bernamurad@users.noreply.github.com>
2021-03-19 12:34:08 +0200

    Added solutions

---
+++ 01.JS-Advanced-Syntax-Functions-and-Statements/Lab/06.js
@@ -0,0 +1,10 @@
+function stars(num) {
+
+    for (let i = 1; i <= num; i++) {
+        console.log('* '.repeat(num));
+    }
+
+
+}
+
+stars(2)
---
+++ 01.JS-Advanced-Syntax-Functions-and-Statements/Lab/08.js
@@ -0,0 +1,18 @@
+function solve(items) {
+    let sum = 0;
+    let sum2 = 0;
+    let concatResult = '';
+    for (let i = 0; i < items.length; i++) {
+        const currentItem = items[i];
+        sum += currentItem;
+        concatResult += currentItem;
+        sum2 += 1 / currentItem;
+    }
+
+    console.log(sum);
+    console.log(sum2);
+    console.log(concatResult);
+
+}
+
+solve([1, 2, 3])
